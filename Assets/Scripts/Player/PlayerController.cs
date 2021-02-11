f48ebcc7683140ec39c43ed8cb1934ae55294d67
Scott Clay <scotteclay2@comcast.net>
2021-02-11 14:02:30 -0700

    added player movement and player controller scirpts, jump check using collision not raycast

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private PlayerMovement _playerMovement;
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _playerMovement = GetComponent<PlayerMovement>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            _playerMovement.UpdateMovement();
+        }
+    }
+}

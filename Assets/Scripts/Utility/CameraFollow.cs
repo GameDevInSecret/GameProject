5a5e4c37a781ca9ffbd4868a8d148eece7fcd507
Scott Clay <scotteclay2@comcast.net>
2021-02-23 14:48:36 -0700

    added CameraFollow utility script

---
+++ Assets/Scripts/Utility/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform followTransform;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerPos = followTransform.position;
+        transform.position = new Vector3(playerPos.x, 0, -10);
+    }
+}

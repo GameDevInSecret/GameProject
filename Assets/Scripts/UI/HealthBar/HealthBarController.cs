c015a4e709211a512e202ce6c86761021d212c74
devpin95 <devpindroid@gmail.com>
2021-03-11 17:57:16 -0700

    Added basic health bar

---
+++ Assets/Scripts/UI/HealthBar/HealthBarController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarController : MonoBehaviour
+{
+    public PlayerAttributes playerAttributes;
+    private Slider _slider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider = GetComponent<Slider>();
+        _slider.maxValue = playerAttributes.initialHealth;
+        _slider.value = _slider.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnHealthEvent()
+    {
+        print("HEALTH BAR: SOMETHISN");
+        _slider.value = playerAttributes.GetHealth();
+    }
+}
